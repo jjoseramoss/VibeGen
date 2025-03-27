c37c28237c58e4eff7360f53ddb88c6f3af2e9a2
jjoseramoss <jr29beto12@gmail.com>
2025-03-26 22:24:59 -0500

    test Spotipy library

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Python Smart Music Playlist Generator for Spotify
 
 This capstone project is meant to get a clearer understanding in Python Object-Oriented Programming Concepts
 
+
+# Setting Up venv and Spotify API
+- 1. Create .venv and install spotipy library
+
 Contributers:
  - Jose Ramos
  - Carlos 
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import config
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config.client_id,
+                                               client_secret=config.secret_client_id,
+                                               redirect_uri=config.redirect_url,
+                                               scope="user-library-read"))
+
+# Simple Code that prints out all of Artist's albums
+taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
+
+results = sp.artist_albums(taylor_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = sp.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
